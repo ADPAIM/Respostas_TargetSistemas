d7d1f6cbd7219231e8db927a2f9c1d2e00ceb59f
ADPAIM <114363763+ADPAIM@users.noreply.github.com>
2023-02-27 23:28:50 -0300

    Create questao-5.js

---
+++ questao-5.js
@@ -0,0 +1,10 @@
+const texto = "Camarada"
+let textoInvertido = ""
+
+for(let i = texto.length-1; i>=0; i--){
+  textoInvertido = textoInvertido + texto[i]
+}
+
+console.log(texto)
+console.log(textoInvertido)
+  
